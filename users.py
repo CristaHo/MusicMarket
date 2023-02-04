fc3f90ddb9154f3eab0ec66238487d278039f779
CristaHo <112627394+CristaHo@users.noreply.github.com>
2023-02-04 12:25:21 +0200

    Add files via upload

---
+++ users.py
@@ -0,0 +1,33 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+def login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone() 
+    if not user:
+        return False
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["user_id"] = user.id
+            return True
+        
+        else:
+            return False
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return True
+
+
+
+
