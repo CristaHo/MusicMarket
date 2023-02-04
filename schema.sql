6b5a64d75fc8554a6ae6b9c74d74b5b7e1404f49
CristaHo <112627394+CristaHo@users.noreply.github.com>
2023-02-04 19:26:51 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
-CREATE TABLE tracks (id SERIAL PRIMARY KEY, artist TEXT, track TEXT, genre TEXT, date TIMESTAMP);
-CREATE TABLE tracks_bought (id SERIAL PRIMARY KEY, user_id TEXT, track_id TEXT);
