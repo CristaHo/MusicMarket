7b1ac426ab6d2f8383ac6a5e1b37378095a8c0f8
Crista Holmström <crista.holmstrom@outlook.com>
2023-02-05 22:38:11 +0200

    Schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 CREATE TABLE tracks (id SERIAL PRIMARY KEY, artist TEXT, track TEXT, genre TEXT, date TIMESTAMP);
+INSERT INTO tracks (artist, track, genre, date) VALUES ('TheArtist', 'TheTrack', 'rock', NOW());
 CREATE TABLE tracks_bought (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER);
