9dc9098cba599b1500a3e026dcb7b86a72e5a343
Crista Holmström <crista.holmstrom@outlook.com>
2023-02-10 10:05:24 +0200

    Price column added to track table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
-CREATE TABLE tracks (id SERIAL PRIMARY KEY, artist TEXT, track TEXT, genre TEXT, date TIMESTAMP);
-INSERT INTO tracks (artist, track, genre, date) VALUES ('TheArtist', 'TheTrack', 'rock', NOW());
+CREATE TABLE tracks (id SERIAL PRIMARY KEY, artist TEXT, track TEXT, genre TEXT, price FLOAT, date TIMESTAMP);
+INSERT INTO tracks (artist, track, genre, date) VALUES ('TheArtist', 'TheTrack', 'rock', 1, NOW());
 CREATE TABLE tracks_bought (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER);
 CREATE TABLE likes (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER);
 CREATE TABLE comments (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER, comment TEXT)
