d14283c51ba63c650f164b193bd22a87ef9ba599
Crista Holmström <crista.holmstrom@outlook.com>
2023-03-05 14:11:00 +0200

    Schema in better form

--- schema.sql
+++ schema.sql
@@ -1,8 +1,38 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
-CREATE TABLE tracks (id SERIAL PRIMARY KEY, artist TEXT, track TEXT, genre TEXT, price FLOAT);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE, 
+    password TEXT
+);
+
+CREATE TABLE tracks (
+    id SERIAL PRIMARY KEY, 
+    artist TEXT, 
+    track TEXT, 
+    genre TEXT, 
+    price FLOAT
+);
+
+CREATE TABLE tracks_bought (
+    id SERIAL PRIMARY KEY, 
+    user_id INTEGER, 
+    track_id INTEGER
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY, 
+    user_id INTEGER, 
+    track_id INTEGER
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY, 
+    user_id INTEGER, 
+    track_id INTEGER, 
+    comment TEXT
+);
+
 INSERT INTO tracks (artist, track, genre, price) VALUES ('TheArtist', 'TheTrack', 'rock', 1);
 INSERT INTO tracks (artist, track, genre, price) VALUES ('BestArtist', 'Mysong', 'dance', 1);
 INSERT INTO tracks (artist, track, genre, price) VALUES ('Popband', 'TrueLove', 'pop', 1);
-CREATE TABLE tracks_bought (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER);
-CREATE TABLE likes (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER);
-CREATE TABLE comments (id SERIAL PRIMARY KEY, user_id INTEGER, track_id INTEGER, comment TEXT);
+
+
